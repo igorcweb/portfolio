a3ab6c1e8df6a0491f9ac687dd6548e4c61f7df7
Igor Chernyshev <igorchern07@gmail.com>
2018-07-30 01:45:07 -0500

    sliding menu, branding, portrait

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,30 +7,6 @@ const navItems = document.querySelectorAll('.nav-item');
 //Set initial state of the menu
 let showMenu = false;
 
-// function toggleMenu() {
-//   if (!showMenu) {
-//     menuBtn.classList.add('close');
-//     menu.classList.add('show');
-//     menuNav.classList.add('show');
-//     branding.classList.add('show');
-//     navItems.forEach(item => {
-//       item.classList.add('show');
-//     });
-
-//     //Set Munu State
-//     showMenu = true;
-//   } else {
-//     menuBtn.classList.remove('close');
-//     menu.classList.remove('show');
-//     menuNav.classList.remove('show');
-//     branding.classList.remove('show');
-//     navItems.forEach(item => {
-//       item.classList.remove('show');
-//     });
-//     //Set Munu State
-//     showMenu = false;
-//   }
-// }
 function toggleMenu() {
   menuBtn.classList.toggle('close');
   menu.classList.toggle('show');
@@ -40,7 +16,7 @@ function toggleMenu() {
     item.classList.toggle('show');
   });
 
-  //Toglle Munu State
+  //Toggle Munu State
   showMenu = !showMenu;
 }
 
